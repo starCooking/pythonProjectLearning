95898a46b393a1ade56f7a658a7a617933eb790d
zhongjingyang <15507025107forlier@gmail.com>
2023-02-17 19:19:27 +0800

    变更

--- test.py
+++ test.py
@@ -1,73 +1,95 @@
-﻿import turtle
-import time
+﻿class Content:
+    """Common base class for all articles/pages"""
 
-# 设置屏幕
-wn = turtle.Screen()
-wn.title("贪吃蛇游戏")
-wn.bgcolor("green")
-wn.setup(width=600, height=600)
+    def __init__(self, topic, url, title, body):
+        self.topic = topic
+        self.title = title
+        self.body = body
+        self.url = url
 
-# 设置蛇头
-head = turtle.Turtle()
-head.speed(0)
-head.shape("square")
-head.color("black")
-head.penup()
-head.goto(0,0)
-head.direction = "stop"
+    def print(self):
+        """
+        Flexible printing function controls output
+        """
+        print('New article found for topic: {}'.format(self.topic))
+        print('URL: {}'.format(self.url))
+        print('TITLE: {}'.format(self.title))
+        print('BODY:\n{}'.format(self.body))
 
-# 设置食物
-food = turtle.Turtle()
-food.speed(0)
-food.shape("circle")
-food.color("red")
-food.penup()
-food.goto(100,100)
 
-# 设置移动函数
-def move():
-    if head.direction == "up":
-        y = head.ycor()
-        head.sety(y + 20)
-    if head.direction == "down":
-        y = head.ycor()
-        head.sety(y - 20)
-    if head.direction == "left":
-        x = head.xcor()
-        head.setx(x - 20)
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x + 20)
+class Website:
+    """Contains information about website structure"""
 
-# 设置控制函数
-def go_up():
-    head.direction = "up"
+    def __init__(self, name, url, searchUrl, resultListing, resultUrl, absoluteUrl, titleTag, bodyTag):
+        self.name = name
+        self.url = url
+        self.searchUrl = searchUrl
+        self.resultListing = resultListing
+        self.resultUrl = resultUrl
+        self.absoluteUrl = absoluteUrl
+        self.titleTag = titleTag
+        self.bodyTag = bodyTag
 
-def go_down():
-    head.direction = "down"
 
-def go_left():
-    head.direction = "left"
+import requests
+from bs4 import BeautifulSoup
 
-def go_right():
-    head.direction = "right"
 
-# 绑定控制函数
-wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "d")
+class Crawler:
 
-# 主循环
-while True:
-    wn.update()
-    # 如果吃到食物
-    if head.distance(food) < 20:
-        x = food.xcor()
-        y = food.ycor()
-        food.goto(x,y)
-    move()
-    time.sleep(0.1)
+    def getPage(self, url):
+        try:
+            req = requests.get(url)
+        except requests.exceptions.RequestException:
+            return None
+        return BeautifulSoup(req.text, 'html.parser')
 
-wn.mainloop()
+    def safeGet(self, pageObj, selector):
+        childObj = pageObj.select(selector)
+        if childObj is not None and len(childObj) > 0:
+            return childObj[0].get_text()
+        return ''
+
+    def search(self, topic, site):
+        """
+        Searches a given website for a given topic and records all pages found
+        """
+        bs = self.getPage(site.searchUrl + topic)
+        searchResults = bs.select(site.resultListing)
+        for result in searchResults:
+            url = result.select(site.resultUrl)[0].attrs['href']
+            # Check to see whether it's a relative or an absolute URL
+            if(site.absoluteUrl):
+                bs = self.getPage(url)
+            else:
+                bs = self.getPage(site.url + url)
+            if bs is None:
+                print('Something was wrong with that page or URL. Skipping!')
+                return
+            title = self.safeGet(bs, site.titleTag)
+            body = self.safeGet(bs, site.bodyTag)
+            if title != '' and body != '':
+                content = Content(topic, title, body, url)
+                content.print()
+
+
+crawler = Crawler()
+
+siteData = [
+    ['O\'Reilly Media', 'http://oreilly.com', 'https://ssearch.oreilly.com/?q=',
+        'article.product-result', 'p.title a', True, 'h1', 'section#product-description'],
+    ['Reuters', 'http://reuters.com', 'http://www.reuters.com/search/news?blob=', 'div.search-result-content',
+        'h3.search-result-title a', False, 'h1', 'div.StandardArticleBody_body_1gnLA'],
+    ['Brookings', 'http://www.brookings.edu', 'https://www.brookings.edu/search/?s=',
+        'div.list-content article', 'h4.title a', True, 'h1', 'div.post-body']
+]
+sites = []
+for row in siteData:
+    sites.append(Website(row[0], row[1], row[2],
+                         row[3], row[4], row[5], row[6], row[7]))
+
+topics = ['python', 'data science']
+for topic in topics:
+    print('GETTING INFO ABOUT: ' + topic)
+    for targetSite in sites:
+        crawler.search(topic, targetSite)
